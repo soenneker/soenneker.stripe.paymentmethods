fa305a8d0c8d41352c236db2f55c4de48fec2dc6
Jake Soenneker <jake@soenneker.com>
2025-12-19 16:15:15 -0600

    Deps

--- src/StripePaymentMethodsUtil.cs
+++ src/StripePaymentMethodsUtil.cs
@@ -19,9 +19,10 @@ public sealed class StripePaymentMethodsUtil : IStripePaymentMethodsUtil
 
     public StripePaymentMethodsUtil(IStripeClientUtil stripeUtil)
     {
-        _service = new AsyncSingleton<PaymentMethodService>(async (cancellationToken, _) =>
+        _service = new AsyncSingleton<PaymentMethodService>(async cancellationToken =>
         {
-            StripeClient client = await stripeUtil.Get(cancellationToken).NoSync();
+            StripeClient client = await stripeUtil.Get(cancellationToken)
+                                                  .NoSync();
 
             return new PaymentMethodService(client);
         });
@@ -30,19 +31,23 @@ public sealed class StripePaymentMethodsUtil : IStripePaymentMethodsUtil
     public async ValueTask<PaymentMethod> Create(PaymentMethodCreateOptions options, RequestOptions? requestOptions = null,
         CancellationToken cancellationToken = default)
     {
-        PaymentMethodService service = await _service.Get(cancellationToken).NoSync();
+        PaymentMethodService service = await _service.Get(cancellationToken)
+                                                     .NoSync();
 
-        return await service.CreateAsync(options, requestOptions, cancellationToken).NoSync();
+        return await service.CreateAsync(options, requestOptions, cancellationToken)
+                            .NoSync();
     }
 
     public async ValueTask<List<PaymentMethod>?> GetAll(CancellationToken cancellationToken = default)
     {
-        IAsyncEnumerable<PaymentMethod>? response = (await _service.Get(cancellationToken).NoSync()).ListAutoPagingAsync(cancellationToken: cancellationToken);
+        IAsyncEnumerable<PaymentMethod>? response = (await _service.Get(cancellationToken)
+                                                                   .NoSync()).ListAutoPagingAsync(cancellationToken: cancellationToken);
 
         if (response == null)
             return null;
 
-        return await response.ToListAsync(cancellationToken).NoSync();
+        return await response.ToListAsync(cancellationToken)
+                             .NoSync();
     }
 
     public async ValueTask<List<PaymentMethod>?> GetAllByUserIdAndType(string userId, StripePaymentMethodType type,
@@ -54,20 +59,22 @@ public sealed class StripePaymentMethodsUtil : IStripePaymentMethodsUtil
             Customer = userId
         };
 
-        IAsyncEnumerable<PaymentMethod>? response =
-            (await _service.Get(cancellationToken).NoSync()).ListAutoPagingAsync(options: options, cancellationToken: cancellationToken);
+        IAsyncEnumerable<PaymentMethod>? response = (await _service.Get(cancellationToken)
+                                                                   .NoSync()).ListAutoPagingAsync(options: options, cancellationToken: cancellationToken);
 
         if (response == null)
             return null;
 
-        return await response.ToListAsync(cancellationToken).NoSync();
+        return await response.ToListAsync(cancellationToken)
+                             .NoSync();
     }
 
     public async ValueTask<List<PaymentMethod>> GetAllByUserIdAndTypes(string userId, List<StripePaymentMethodType> types,
         CancellationToken cancellationToken = default)
     {
         var all = new List<PaymentMethod>();
-        PaymentMethodService service = await _service.Get(cancellationToken).NoSync();
+        PaymentMethodService service = await _service.Get(cancellationToken)
+                                                     .NoSync();
 
         foreach (StripePaymentMethodType type in types)
         {
@@ -77,8 +84,9 @@ public sealed class StripePaymentMethodsUtil : IStripePaymentMethodsUtil
                 Customer = userId
             };
 
-            List<PaymentMethod>? methods =
-                await service.ListAutoPagingAsync(options, cancellationToken: cancellationToken).ToListAsync(cancellationToken).NoSync();
+            List<PaymentMethod>? methods = await service.ListAutoPagingAsync(options, cancellationToken: cancellationToken)
+                                                        .ToListAsync(cancellationToken)
+                                                        .NoSync();
 
             if (methods != null)
                 all.AddRange(methods);
@@ -96,8 +104,10 @@ public sealed class StripePaymentMethodsUtil : IStripePaymentMethodsUtil
             Limit = 1
         };
 
-        PaymentMethodService service = await _service.Get(cancellationToken).NoSync();
-        StripeList<PaymentMethod>? list = await service.ListAsync(options, cancellationToken: cancellationToken).NoSync();
+        PaymentMethodService service = await _service.Get(cancellationToken)
+                                                     .NoSync();
+        StripeList<PaymentMethod>? list = await service.ListAsync(options, cancellationToken: cancellationToken)
+                                                       .NoSync();
 
         return list?.Data?.FirstOrDefault();
     }
@@ -105,30 +115,35 @@ public sealed class StripePaymentMethodsUtil : IStripePaymentMethodsUtil
     public async ValueTask<PaymentMethod> Update(string paymentMethodId, PaymentMethodUpdateOptions options, RequestOptions? requestOptions = null,
         CancellationToken cancellationToken = default)
     {
-        PaymentMethodService service = await _service.Get(cancellationToken).NoSync();
+        PaymentMethodService service = await _service.Get(cancellationToken)
+                                                     .NoSync();
 
-        return await service.UpdateAsync(paymentMethodId, options, requestOptions, cancellationToken).NoSync();
+        return await service.UpdateAsync(paymentMethodId, options, requestOptions, cancellationToken)
+                            .NoSync();
     }
 
     public async ValueTask<PaymentMethod?> Get(string paymentMethodId, PaymentMethodGetOptions? options = null, RequestOptions? requestOptions = null,
         CancellationToken cancellationToken = default)
     {
-        return await (await _service.Get(cancellationToken).NoSync()).GetAsync(paymentMethodId, options, requestOptions, cancellationToken: cancellationToken)
-            .NoSync();
+        return await (await _service.Get(cancellationToken)
+                                    .NoSync()).GetAsync(paymentMethodId, options, requestOptions, cancellationToken: cancellationToken)
+                                              .NoSync();
     }
 
     public async ValueTask Delete(string paymentMethodId, PaymentMethodDetachOptions? options = null, RequestOptions? requestOptions = null,
         CancellationToken cancellationToken = default)
     {
-        await (await _service.Get(cancellationToken).NoSync()).DetachAsync(paymentMethodId, options, requestOptions, cancellationToken: cancellationToken)
-            .NoSync();
+        await (await _service.Get(cancellationToken)
+                             .NoSync()).DetachAsync(paymentMethodId, options, requestOptions, cancellationToken: cancellationToken)
+                                       .NoSync();
     }
 
     public async ValueTask Attach(string paymentMethodId, PaymentMethodAttachOptions options, RequestOptions? requestOptions = null,
         CancellationToken cancellationToken = default)
     {
-        await (await _service.Get(cancellationToken).NoSync()).AttachAsync(paymentMethodId, options, requestOptions, cancellationToken: cancellationToken)
-            .NoSync();
+        await (await _service.Get(cancellationToken)
+                             .NoSync()).AttachAsync(paymentMethodId, options, requestOptions, cancellationToken: cancellationToken)
+                                       .NoSync();
     }
 
     public void Dispose()
